2e5313c7fa7bb430f6e03af568a9d76ef77f16cc
Jesus Medina <medinabotamail@gmail.com>
2020-07-24 01:12:38 -0400

    se hicieron cambios

--- js/main.js
+++ js/main.js
@@ -21,19 +21,16 @@ $(document).ready(function(){
     // MOSTRAR Y OCULTAR PRODUCTOS
     $('.items').hide()
 
+    $('.categorias a').on('click', function(event){
 
+        var enlace = $(this).attr('href')
+        $(enlace).addClass('mostrar-items')
+        $(enlace).slideToggle(1000)
+    })
 
-
-    // $('.categorias a').on('click', function(event){
-
-    //     var enlace = $(this).attr('href')
-    //     $(enlace).addClass('mostrar-items')
-    //     $(enlace).slideToggle(1000)
-    // })
-
-    // $('.banner a').on('click', function(event){
-    //     var enlace = $(this).attr('href')
-    //     $(enlace).addClass('mostrar-items')
-    //     $(enlace).slideToggle(1000)
-    // })
+    $('.banner a').on('click', function(event){
+        var enlace = $(this).attr('href')
+        $(enlace).addClass('mostrar-items')
+        $(enlace).slideToggle(1000)
+    })
 })
