99b8a1e7e92dfa2f04214752127e01619a88aa97
Jesus Medina <medinabotamail@gmail.com>
2020-07-26 19:05:28 -0400

    Colores a categorias y preventdefault a banners

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $('.page').hide()
 $(document).ready(function(){
-    $('.loading').remove()
-    $('.page').fadeIn(500)
+    $('.loading').remove();
+    $('.page').fadeIn(500);
 
 
     // BARRA FIJA - MENU ON TOP
@@ -9,9 +9,9 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if(scroll > alturaLogotipo){
-            $('.barra-fija').addClass('fixed')
+            $('.barra-fija').addClass('fixed');
         } else {
-            $('.barra-fija').removeClass('fixed')
+            $('.barra-fija').removeClass('fixed');
         }
     })
     // MENU MOVIL
@@ -19,29 +19,30 @@ $(document).ready(function(){
     $('.menu-movil').on('click', function(event){
 
         if(img == 'img/open-menu.png'){
-        $('.navegacion').slideDown(1000)
-        $('.menu-movil img').attr('src', 'img/close.png')
+        $('.navegacion').slideDown(1000);
+        $('.menu-movil img').attr('src', 'img/close.png');
         img = $('.menu-movil img').attr('src');
         } else if (img = 'img/close.png'){
-            $('.navegacion').slideUp(1000)
-            $('.menu-movil img').attr('src', 'img/open-menu.png')
+            $('.navegacion').slideUp(1000);
+            $('.menu-movil img').attr('src', 'img/open-menu.png');
             img = $('.menu-movil img').attr('src');
         }
     })
 
     // MOSTRAR Y OCULTAR PRODUCTOS
-    $('.items').hide()
+    $('.items').hide();
 
     $('.categorias a').on('click', function(event){
-
-        var enlace = $(this).attr('href')
-        $(enlace).addClass('mostrar-items')
-        $(enlace).slideToggle(1000)
+        var enlace = $(this).attr('href');
+        $(enlace).addClass('mostrar-items');
+        $(enlace).slideToggle(1000);
     })
 
     $('.banner a').on('click', function(event){
-        var enlace = $(this).attr('href')
-        $(enlace).addClass('mostrar-items')
-        $(enlace).slideToggle(1000)
+        event.preventDefault();
+        var enlace = $(this).attr('href');
+        $(enlace).addClass('mostrar-items');
+        $(enlace).slideToggle(1000);
+
     })
 })
